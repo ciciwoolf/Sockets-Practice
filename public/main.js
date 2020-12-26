d9812586f95145df5b8aceb76199561fdbb6d8b5
OleksiiRomanchenko <Oleksii.Romanchenko@itechart-group.com>
2020-12-26 16:10:16 +0300

    create chat

--- public/main.js
+++ public/main.js
@@ -1,45 +1,29 @@
 var socket = io.connect('http://localhost:8083', {'forceNew': true })
-
+const ul = document.querySelector('.chat');
+const username = document.getElementById('username');
+const message = document.getElementById('text');
  /*getElementByID method gets data from html */
  function addMessage(e) {
-  var message = {
-     author: document.getElementById('username').value,
-     text: document.getElementById('text').value
+  let info = {
+     author: username.value,
+     text: message.value
     };
-    
-    console.log("Here is the", message)
-
-    socket.emit('new-message', message);  //sends to the server main.js, backend
+    if(username.value !== '' || message.value !== '') {
+        socket.emit('new-message', info);  //sends to the server main.js, backend
+    } 
     return false; 
 }
 
 //Receives content of message from server main.js
 socket.on('new-message', function(data){
-    console.log("I am listening for new data", data);   //this works    
-
+    console.log("I am listening for new data", data, ul);
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${data.author} : ${data.text}`;
+    ul.appendChild(listItem);   //this works  
+    username.value = '';
+    message.value = '';
    });
 
-   
-   //1. Create variable which links variable to html element
-   //do we need this again, since we already gathered data above, which was sent to the server?
-   const sentEvent = document.getElementById("Sent");
-  
-  
-   //2. Create a function that occurs in the event handler function
-   function render (data) {             
-    var html = `<div> <strong>${data.author}</strong>: ${data.text}</div>`;  
-    console.log ("Rendering yet?", data);  
-    
-    return document.getElementById('messages').innerHTML = html;
-    
-   }
-
-   //3. Create event handler using above 1 and 2
-
-  
-   //sentEvent.addEventListener('click', render);
-
-   sentEvent.onclick = render; 
 
 
 
